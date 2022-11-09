220ec806c11c84cdcc36825546652553893c1a65
Ameer-Ali-Jamali <ameer.ali.jamali886@gmail.com>
2022-11-09 19:02:06 +0500

    now code will be understand easily, mentioned.-Exp

--- index.php
+++ index.php
@@ -1,76 +1,63 @@
-<!DOCTYPE html>
-<html lang="en">
+PHP Iterative Statements
+Iterative statements are used to run same block of code over and over again for a certain number of times.
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
+In PHP, we have the following loops:
 
-<body>
-
-    <?php
-
-    // PHP Iterative Statements
-    // Iterative statements are used to run same block of code over and over again for a certain number of times. 
-
-    // In PHP, we have the following loops:
-
-    // 1 while Loop
-    // 2 do-while Loop
-    // 3 for Loop
-    // 4 foreach loop
-
-    // 1. While Loop
-    // The While loop in PHP is used when we need to execute a block of code again and again based on a given condition. If the condition never becomes false, the while loop keeps getting executed. Such a loop is known as an infinite loop. 
-
-    // Example:
-
-    $x = 1;
-    while ($x <= 10) {
-        echo "The number is: $x <br>";
-        $x++;
-    }
+while Loop
+do-while Loop
+for Loop
+foreach loop
 
 
-    // 2. Do-While Loop
-    // The do-while loop is similar to a while loop except that it is guaranteed to execute at least once. After executing a part of a program for once, the rest of the code gets executed based on a given boolean condition.
+1. While Loop
+The While loop in PHP is used when we need to execute a block of code again and again based on a given condition. If the condition never becomes false, the while loop keeps getting executed. Such a loop is known as an infinite loop.
 
-    // Example:
+Example:
 
-    $x = 10;
-    do {
-        echo "The number is: $x <br>";
-        $x++;
-    } while ($x <= 9);
-
-
-    // 3. For Loop
-    // The for loop is used to iterate a block of code multiple times. 
-
-    // Example:
-
-    for ($x = 0; $x <= 10; $x++) {
-        echo "The number is: $x <br>";
-    }
+<?php
+$x = 1;
+while ($x <= 10) {
+    echo "The number is: $x <br>";
+    $x++;
+}
+?>
 
 
-    // 4. Foreach loop
-    // The foreach loop in PHP can be used to access the array indexes in PHP. It only works on arrays and objects. 
+2. Do-While Loop
+The do-while loop is similar to a while loop except that it is guaranteed to execute at least once. After executing a part of a program for once, the rest of the code gets executed based on a given boolean condition.
 
-    // Example:
+Example:
 
-    echo "Welcome to the world of foreach loops <br>";
-    $arr = array("Bananas", "Apples", "Harpic", "Bread", "Butter");
-    foreach ($arr as  $value) {
-        echo "$value <br>";
-    }
+<?php
+$x = 10;
+do {
+    echo "The number is: $x <br>";
+    $x++;
+} while ($x <= 9);
+?>
 
-    ?>
+
+3. For Loop
+The for loop is used to iterate a block of code multiple times.
+
+Example:
+
+<?php
+for ($x = 0; $x <= 10; $x++) {
+    echo "The number is: $x <br>";
+}
+?>
 
 
+4. Foreach loop
+The foreach loop in PHP can be used to access the array indexes in PHP. It only works on arrays and objects.
 
-</body>
+Example:
 
-</html>
+<?php
+echo "Welcome to the world of foreach loops <br>";
+$arr = array("Bananas", "Apples", "Harpic", "Bread", "Butter");
+foreach ($arr as  $value) {
+    echo "$value <br>";
+}
+?>
